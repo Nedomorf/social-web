c82b61c2fde9003c72a1a7d5812acc8166060812
Nedomorf <danikp01@,ail.ru>
2020-09-24 20:18:30 +0300

    Updates

---
+++ precache-manifest.212e6d22e4cccb2aa4b3d0187e452225.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09e5bb00efc0b487abe0c251aa9568d",
+    "url": "/social-web/index.html"
+  },
+  {
+    "revision": "da17f7a0530ca82e3c82",
+    "url": "/social-web/static/css/main.6d8bc109.chunk.css"
+  },
+  {
+    "revision": "d3ab19cc95f0b6fa4a04",
+    "url": "/social-web/static/js/2.45c67be0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-web/static/js/2.45c67be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da17f7a0530ca82e3c82",
+    "url": "/social-web/static/js/main.c16b1bf5.chunk.js"
+  },
+  {
+    "revision": "9f9b15dac6eb7f54c78c",
+    "url": "/social-web/static/js/runtime-main.81c02c6a.js"
+  },
+  {
+    "revision": "9253235bf5320999b3e3f3cc28ecb66d",
+    "url": "/social-web/static/media/Ann.9253235b.jpg"
+  },
+  {
+    "revision": "15f33404f44be012c4ae51499b611366",
+    "url": "/social-web/static/media/git-profile.15f33404.jpg"
+  },
+  {
+    "revision": "f066f360ab836bc057ebc54e268491fd",
+    "url": "/social-web/static/media/preloader.f066f360.svg"
+  }
+]);
